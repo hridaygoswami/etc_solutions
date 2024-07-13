79fb3ddc58c0a2028add01bc5d5bc30e9654641a
hridaygoswami <epikhriday@gmail.com>
2024-07-13 18:39:40 +0530

    adding the new files

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='Index')
+    path("", views.index, name='Index'),
+    path("about/", views.about, name='About'),
+    path("contact/", views.contact, name='Contact'),
+    path("gallery/", views.gallery, name='Gallery'),
+    path("services/", views.services, name='Services'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def gallery(request):
+    return render(request, "gallery.html")
+
+def services(request):
+    return render(request, "services.html")
